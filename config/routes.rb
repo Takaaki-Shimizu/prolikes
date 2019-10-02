717d3ea78235bc36be7ae6e5c5629c8ad10b929d
Takaaki-Shimizu <57.shimizu.takaaki@gmail.com>
2019-10-02 13:35:08 +0900

    良いね機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
   resources :posts do
+    resources :comments, only: [:create]
+  end
+  resources :posts do
     resources :users
   end
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
+  get "users/:id/likes" => "users#likes"
 end
